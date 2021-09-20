5ed21b4ea1aabb0f01627555a9d96d3c541fea9a
LemonThumps <91098507+LemonThumps@users.noreply.github.com>
2021-09-20 17:39:14 -0500

    Add initial files

---
+++ index.js
@@ -0,0 +1,40 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+function randomDice1() {
+  var diceImage1 = getElementsByClassName("img1")[1]
+  if (randomDice1() === 1) {
+    diceImage1.setAttribute(src="dice1.png")
+  }
+  else if (randomDice1() === 2) {
+    diceImage1.setAttribute(src="dice2.png")
+  }
+  else if (randomDice1() === 3) {
+    diceImage1.setAttribute(src="dice3.png")
+  }
+  else if (randomDice1() === 4) {
+    diceImage1.setAttribute(src="dice4.png")
+  }
+  else if (randomDice1() === 5) {
+    diceImage1.setAttribute(src="dice5.png")
+  }
+}
+
+function randomDice2() {
+  var diceImage2 = getElementsByClassName("img2")[1]
+  if (randomDice2() === 1) {
+    diceImage2.setAttribute(src="dice1.png")
+  }
+  else if (randomDice2() === 2) {
+    diceImage2.setAttribute(src="dice2.png")
+  }
+  else if (randomDice2() === 3) {
+    diceImage2.setAttribute(src="dice3.png")
+  }
+  else if (randomDice2() === 4) {
+    diceImage2.setAttribute(src="dice4.png")
+  }
+  else if (randomDice2() === 5) {
+    diceImage2.setAttribute(src="dice5.png")
+  }
+}
